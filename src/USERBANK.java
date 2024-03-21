c36eed33fcfbf982249d61c6ef5940591f551e88
hlayiseko bennet <nhlayisekobennet07@gmail.com>
2024-03-21 10:31:48 +0200

    completed task

---
+++ src/USERBANK.java
@@ -0,0 +1,33 @@
+public class USERBANK {
+    private double balance;
+
+    public USERBANK(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance = balance + amount;
+            System.out.println("\tsuccessfully deposited you account. \nNew balance: R" + balance);
+        }
+
+        else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance =balance- amount;
+            System.out.println("\tYour Withdrawal was a success.\n New balance: R" + balance);
+        }
+
+        else {
+            System.out.println("Insufficient funds or invalid withdrawal amount.");
+        }
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+}
